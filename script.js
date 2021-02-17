d534a0ff175a511126cd329b14f5d48f6e72e8ba
Deivid Teta <dteta890@gmail.com>
2021-02-17 23:01:51 +0100

    Basic Error handling based on API response

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ $(document).ready(function () {
         let url =
             "https://api.cryptonator.com/api/ticker/" + base + "-" + target;
         $.getJSON(url, function (data) {
-            $("#pairPrice").text(parseFloat(data.ticker.price).toFixed(2));
+            if (data.success) {
+                $("#pairPrice")
+                    .text("Price: " + parseFloat(data.ticker.price).toFixed(4))
+                    .css("color", "green");
+            } else {
+                $("#pairPrice")
+                    .text("Error: " + data.error)
+                    .css("color", "red");
+            }
         });
     });
 });
+
+// Populate dropdown with supported currencies (only status primary) limit shown items and search function API = https://www.cryptonator.com/api/currencies TUT = https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
+// Add the calculating function
+// Add switch sides function
