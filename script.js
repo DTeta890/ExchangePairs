12e6c7324ba71f7dcf60b768e662ff407770dd0c
Deivid Teta <dteta890@gmail.com>
2021-02-18 11:29:14 +0100

    Populated dropdowns with supported currencies (only status primary) from API

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
+    // Stops the form submit to reload the page
     $("#formPairs").submit(function (e) {
         e.preventDefault();
     });
+
+    // Populating base and target dropdowns with currencies from API
+    let baseDropdown = $("#base");
+    let targetDropdown = $("#target");
+    let pairDropdown = $(".pairDropdown");
+    pairDropdown.empty();
+    pairDropdown.append("<option selected='true' disabled>Choose Currency</option>");
+    pairDropdown.prop("selectedIndex", 0);
+    $.getJSON("https://www.cryptonator.com/api/currencies", function (data) {
+        $.each(data.rows, function (i, item) {
+            // Including only primary cyrrencies
+            if ($.inArray("primary", item.statuses) != -1) {
+                pairDropdown.append($("<option></option>").attr("value", item.code).text(item.name));
+            }
+        });
+    });
+
     $("#submitBtn").click(function showRates() {
         let base = $("#base").val();
         let target = $("#target").val();
-        let url =
-            "https://api.cryptonator.com/api/ticker/" + base + "-" + target;
+        // API link is formed by base and target values
+        let url = "https://api.cryptonator.com/api/ticker/" + base + "-" + target;
         $.getJSON(url, function (data) {
             if (data.success) {
                 $("#pairPrice")
@@ -21,6 +39,6 @@ $(document).ready(function () {
     });
 });
 
-// Populate dropdown with supported currencies (only status primary) limit shown items and search function API = https://www.cryptonator.com/api/currencies TUT = https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
+// Add loading page 3sec before showing the page to give APIs time to load
 // Add the calculating function
 // Add switch sides function
